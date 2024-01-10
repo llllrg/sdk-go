2e6522659f965fa646ce8906b1dfa1850f70ff94
luorengui <rengui.luo@selan.tech>
2024-01-10 16:37:15 +0800

    feat:添加管理员发送邮件功能

--- nauth/admin_user.go
+++ nauth/admin_user.go
@@ -130,3 +130,31 @@ func (cli *AuthenticationAdmin) PostSMS(phone string, content string) error {
 	return nil
 
 }
+
+func (c *AuthenticationAdmin) PostEmail(email string, content string, scene int) error {
+
+	body, err := c.SendHttpRequest("/user/email", http.MethodPost, map[string]interface{}{
+		"email":   email,
+		"content": content,
+		"scene":   scene,
+	})
+	if err != nil {
+		return err
+	}
+
+	var p fastjson.Parser
+	v, err := p.Parse(string(body))
+	if err != nil {
+		return err
+	}
+
+	if !v.GetBool("status") {
+		msg := v.GetStringBytes("message")
+		if err != nil {
+			return err
+		}
+		return errors.New(string(msg))
+	}
+
+	return nil
+}
